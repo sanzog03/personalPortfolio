638953f2669ccfebf7fe0dc2c4493458e063172b
Sanjog Thapa <sanzog03@gmail.com>
2023-08-24 18:33:44 -0500

    photo updated. theme updated gh-pages upload script added build added to git ignore.

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Sanjog Thapa',
     title: "Software Engineer",
     desciption:"I'm a dedicated software engineer pursuing a Master's in Computer Science at the University of Alabama in Huntsville. As a Graduate Research Assistant at NASA GHRC DAAC, I focus on cloud-based tools and advanced visualizations using Cesium, AWS, serverless architecture, React, Python, and Terraform. I've also led projects in cloud based digital signage solutions and spectrometry based health management software as a Software Engineer at Gurzu Inc. With a passion for tech innovation, I aim for impactful results.",
-    image: 'https://avatars.githubusercontent.com/u/38744044?s=400&u=88353e51abbcba28daf7d2e8c1a3f94622af8ac5&v=4',
+    image: 'https://scontent-atl3-1.xx.fbcdn.net/v/t1.6435-9/48944753_2785366728140917_7267345910512746496_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=174925&_nc_ohc=Ey5W_9oFsGMAX_50Swf&_nc_ht=scontent-atl3-1.xx&oh=00_AfDS9v3unDC3mSb29Tx4Y3_so2BOFYxpQ4WYbK-pEYdCJg&oe=650F543B',
     resumePdf: resume
 }
