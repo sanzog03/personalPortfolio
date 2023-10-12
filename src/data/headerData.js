5c5612f40c93792697bc66e97970665568f990e6
Sanjog Thapa <sanzog03@gmail.com>
2023-10-12 18:07:54 -0500

    favicon changed. Dp stored locally in public

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Sanjog Thapa',
     title: "Software Engineer",
     desciption:"I’m a Software developer who enjoys the process of problem-solving and turning ideas into reality. I’m always looking to expand my knowledge, improve upon old skills, and learn new ones. For the past 5 years, I’ve been working with JavaScript, TypeScript, NodeJS, and Python to help bring about solutions for our company’s internal, as well as outside. Currently, I am pursuing a Master's in Computer and Information Science as a Graduate Research Assistant specializing in data mining, simulations, and cloud.",
-    image: 'https://scontent-atl3-1.xx.fbcdn.net/v/t1.6435-9/48944753_2785366728140917_7267345910512746496_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=174925&_nc_ohc=Ey5W_9oFsGMAX_50Swf&_nc_ht=scontent-atl3-1.xx&oh=00_AfDS9v3unDC3mSb29Tx4Y3_so2BOFYxpQ4WYbK-pEYdCJg&oe=650F543B',
+    image: '/dp.jpg',
     resumePdf: resume
 }
