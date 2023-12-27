afa5ea721f11aafb384093cf2717b57440314e67
Sanjog Thapa <sanzog03@gmail.com>
2023-12-27 14:36:28 -0600

    spelling fix

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ import resume from '../assets/pdf/Sanjog_Thapa_Resume.pdf'
 export const headerData = {
     name: 'Sanjog Thapa',
     title: "Software Engineer",
-    desciption:"I’m a Software Engineer with 5 years of experience. I enjoy problem-solving and turning ideas into reality and have consistently showcased my coding proficiency and familiarity with architectural patterns for large, reliable, and highly scalable applications in Earth Data Science, Health, and Digital Advertisement domains. I have just completed my Master's in Computer and Information Science, and I am currently looking for a Full-time job opportunitiy as a Software Engineer.",
+    desciption:"I’m a Software Engineer with 5 years of experience. I enjoy problem-solving and turning ideas into reality and have consistently showcased my coding proficiency and familiarity with architectural patterns for large, reliable, and highly scalable applications in Earth Data Science, Health, and Digital Advertisement domains. I have just completed my Master's in Computer and Information Science, and I am currently looking for a Full-time job opportunity as a Software Engineer.",
     image: '/dp.jpeg',
     resumePdf: resume
 }
