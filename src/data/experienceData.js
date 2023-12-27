d085b718f7e563ce1a58d23c074ef633e37a196a
Sanjog Thapa <sanzog03@gmail.com>
2023-12-27 12:24:55 -0600

    updated header intro. Also updated experience section.

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -2,29 +2,29 @@ export const experienceData = [
     {
         id: 1,
         company: 'NASA GHRC DAAC, ITSC UAH',
-        jobtitle: 'Full Stack / Graduate Research Assistant',
-        startYear: '2022',
-        endYear: 'Present'
+        jobtitle: 'Graduate Research Assistant - Data Mining and Web Service',
+        startYear: 'Aug 2022',
+        endYear: 'Dec 2023'
     },
     {
         id: 2,
         company: 'GURZU INC',
-        jobtitle: 'Software Engineer',
-        startYear: '2020',
-        endYear: '2022'
+        jobtitle: 'Software Engineer - Full Stack Web Services',
+        startYear: 'Aug 2020',
+        endYear: 'Aug 2022'
     },
     {
         id: 3,
         company: 'JYAASA TECHNOLOGIES',
-        jobtitle: 'Associate Software Engineer',
-        startYear: '2019',
-        endYear: '2020'
+        jobtitle: 'Associate Software Engineer - Frontend and Desktop App',
+        startYear: 'Jan 2019',
+        endYear: 'Aug 2020'
     },
     {
         id: 4,
         company: 'TECHNORIO INC',
-        jobtitle: 'Internship',
-        startYear: '2018',
-        endYear: '2018'
+        jobtitle: 'Internship - Javascript',
+        startYear: 'Sep 2018',
+        endYear: 'Dec 2018'
     },
 ]
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ import resume from '../assets/pdf/resume.pdf'
 export const headerData = {
     name: 'Sanjog Thapa',
     title: "Software Engineer",
-    desciption:"I’m a Software developer who enjoys the process of problem-solving and turning ideas into reality. I’m always looking to expand my knowledge, improve upon old skills, and learn new ones. For the past 5 years, I’ve been working with JavaScript, TypeScript, NodeJS, and Python to help bring about solutions for our company’s internal, as well as outside. Currently, I am pursuing a Master's in Computer and Information Science as a Graduate Research Assistant specializing in data mining, simulations, and cloud.",
+    desciption:"I’m a Software Engineer with 5 years of experience. I enjoy problem-solving and turning ideas into reality and have consistently showcased my coding proficiency and familiarity with architectural patterns for large, reliable, and highly scalable applications in Earth Data Science, Health, and Digital Advertisement domains. I have just completed my Master's in Computer and Information Science, and I am currently looking for a Full-time job opportunitiy as a Software Engineer.",
     image: '/dp.jpg',
     resumePdf: resume
 }
